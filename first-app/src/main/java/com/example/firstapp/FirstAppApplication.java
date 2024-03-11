b04268a568a8eed6de7c6308944d0c0bd50b8c68
Aarón García Lezama <aaron.garcia.94@icloud.com>
2024-03-10 23:28:35 -0600

    Add: Se crea metodo con tipos de datos [tipoDatos()].

--- first-app/src/main/java/com/example/firstapp/FirstAppApplication.java
+++ first-app/src/main/java/com/example/firstapp/FirstAppApplication.java
@@ -12,5 +12,47 @@ public class FirstAppApplication {
 		System.out.println("\n\n\n");
     	System.out.println("String: HOLA MUNDO!!!");
     	
-	}	
+    	TipoDatos();
+    	
+	}
+	
+	public static void TipoDatos() {
+		// **Tipos de datos primitivos:**
+			// Enteros
+			byte    numeroByte      = 127;                      // Rango: -128 a 127
+			short   numeroShort     = 32767;                    // Rango: -32768 a 32767
+			int     numeroInt       = 2147483647;               // Rango: -2147483648 a 2147483647
+			long    numeroLong      = 9223372036854775807L;		// Rango: -9223372036854775808 a 9223372036854775807
+			
+			// Decimales
+			float   numeroFloat     = 3.1415926535f;        	// Rango: -3.4028235E38 a 3.4028235E38
+			double  numeroDouble    = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679;
+			                                                	// Rango: -1.7976931348623157E308 a 1.7976931348623157E308
+			// Caracteres
+			char    caracter        = 'a';
+			
+			// Booleanos
+			boolean verdadero       = true;
+			boolean falso           = false;
+		
+			// **Impresiones en consola:**
+			System.out.println("\n**Tipos de datos primitivos:**");
+			System.out.println("Byte: " + numeroByte);
+			System.out.println("Short: " + numeroShort);
+			System.out.println("Int: " + numeroInt);
+			System.out.println("Long: " + numeroLong);
+			System.out.println("Float: " + numeroFloat);
+			System.out.println("Double: " + numeroDouble);
+			System.out.println("Caracter: " + caracter);
+			System.out.println("Verdadero: " + verdadero);
+			System.out.println("Falso: " + falso);
+			
+		// **Tipos de datos no primitivos:**
+			// Cadenas
+			String nombre = "Aarón García Lezama";
+			
+			// **Impresiones en consola:**
+			System.out.println("\n**Tipo de dato no primitivo:**");
+			System.out.println("String: " + nombre);
+	    }
 }
